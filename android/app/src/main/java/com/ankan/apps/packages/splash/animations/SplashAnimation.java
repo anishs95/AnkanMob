9e11f7dbd1cb86046c911b83e6b6323cfc412245
anishs95 <anish.s95@gmail.com>
2022-02-22 23:11:29 +0530

    first commit

---
+++ android/app/src/main/java/com/ankan/apps/packages/splash/animations/SplashAnimation.java
@@ -0,0 +1,15 @@
+package com.ankan.apps.packages.splash.animations;
+
+import android.view.animation.Animation;
+
+public interface SplashAnimation {
+  Animation create(Config config);
+
+  class Config {
+    int duration;
+
+    public Config(int duration) {
+      this.duration = duration;
+    }
+  }
+}
